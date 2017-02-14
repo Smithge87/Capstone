9181d6cbbfadbc61cd238557d9970b757ec97317
Grant Smith <smithge87@gmail.com>
2017-02-14 06:49:52 -0600

    implemented adding logo to organization side

--- VolunteerWebApp/VolunteerWebApp/Controllers/OrganizationController.cs
+++ VolunteerWebApp/VolunteerWebApp/Controllers/OrganizationController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Mvc;
 using VolunteerWebApp.Models;
 
@@ -112,6 +114,38 @@ namespace VolunteerWebApp.Controllers
 
             }
             return View(model);
+        }
+        public ActionResult AddPhoto()
+        {
+            var currentUserName = User.Identity.Name;
+            var currentUser = _context.Users.FirstOrDefault(m => m.UserName == currentUserName);
+            var viewModel = new PhotoViewModel()
+            {
+                ApplicationUser = currentUser
+            };
+            return View(viewModel);
+        }
+        [HttpPost]
+
+        public ActionResult AddPhoto(PhotoViewModel model, FormCollection form)
+        {
+            var currentUserName = User.Identity.Name;
+            var currentUser = _context.Users.FirstOrDefault(m => m.UserName == currentUserName);
+            WebImage photo = null;
+            var newFileName = "";
+            var imagePath = "";
+            photo = WebImage.GetImageFromRequest();
+            if (photo != null)
+            {
+                newFileName = Guid.NewGuid().ToString() + "_" +
+                Path.GetFileName(photo.FileName);
+                imagePath = @"images\" + newFileName;
+
+                photo.Save(@"~\" + imagePath);
+                currentUser.ProfilePhoto = ("../" + imagePath);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Index", "Organization");
         }
     }
 
